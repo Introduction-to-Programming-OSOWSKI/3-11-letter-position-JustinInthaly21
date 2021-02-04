0af8b57f4bf2c6c541344cfeb296b1fdc58021c7
JustinInthaly21 <825134@mystma.org>
2021-02-04 19:03:39 +0000

    letterpos2

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def letterPos(w, x):
     
-
+    a = None
     
     for i in range(0, len(w)):
         if w[i] == x:
-            return i + 1
-        else:
-            return None
+            a = i + 1
+            break
+
+    if a == None:
+        return "No"
+    else:
+        return a
+         
       
 
-print(letterPos("hello", "h"))
+print(letterPos("hello", "l"))
 
